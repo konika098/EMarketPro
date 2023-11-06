850520973dea8d5c1e4170c505421159d8f07e4e
konika098 <konikaakter2000@gmail.com>
2023-11-06 21:14:02 +0600

    add headerome in the header

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -2,21 +2,25 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Component/Header/Header";
 import Footer from "../Layout/Footer/Footer";
+import Headroom from "react-headroom";
 
 
 
 const Root = () => {
-    return (
-      <>
-      <Header/>
-      <Outlet/>
-    <div className="mt-10">
-    <Footer/>
-    </div>
-   
-      
-      </>
-    );
+  return (
+    <>
+
+      <Headroom>
+        <Header />
+      </Headroom>
+      <Outlet />
+      <div className="mt-10">
+        <Footer />
+      </div>
+
+
+    </>
+  );
 };
 
 export default Root;
